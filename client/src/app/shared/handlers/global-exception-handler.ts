ac91115405c54a40d9ab785f085c415f1cf53404
sshibani <siawash.shibani@trivident.com>
2017-02-11 12:49:02 +0100

    implement global exception handling.

---
+++ client/src/app/shared/handlers/global-exception-handler.ts
@@ -0,0 +1,28 @@
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+
+import { MessageService } from './../../services/message.service';
+
+@Injectable()
+export class GlobalExceptionHandler implements ErrorHandler {
+
+  private messageService: MessageService;
+
+  constructor(private injector: Injector) {
+     this.messageService = injector.get(MessageService);
+  }
+
+  handleError(error) {
+    this.messageService.SendMessage("danger", error, 10000);
+  }
+
+}
+
+// @NgModule({
+//   providers: [
+//     {
+//       provide: ErrorHandler,
+//       useClass: GlobalExceptionHandler
+//     }
+//   ]
+// })
+// export class AppModule { }
