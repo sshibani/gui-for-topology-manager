61807978ff79da6b45f0f78a7729c2a31b21799c
sshibani <siawash.shibani@trivident.com>
2017-04-12 17:49:23 +0200

    moved environment specific variables to webpack config files.

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -1,7 +1,24 @@
 const path = require('path');
+const { DefinePlugin } = require('webpack');
+
+const env = {
+  production: false,
+  // localEndPoint: "http://34.249.176.174:99/api/"
+  // localEndPoint: "/assets/data/"
+  localEndPoint: 'http://localhost:99/api/',
+  showDebugMessages: true
+}
 
 module.exports = {
   output: {
     path: path.join(process.cwd(), "dist"),
   },
+   plugins: [
+     new DefinePlugin({
+        'process.env': {
+          'API_URL': JSON.stringify(env.localEndPoint),
+          'production': env.production
+        }
+     })
+   ],
 };
--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -1,7 +1,23 @@
 const path = require('path');
+const { DefinePlugin } = require('webpack');
+
+const env = {
+  production: true,
+  localEndPoint: '/api/',
+  showDebugMessages: true
+};
+
 
 module.exports = {
   output: {
     path: path.join(process.cwd(), "/../build"),
   },
+   plugins: [
+     new DefinePlugin({
+        'process.env': {
+          'API_URL': JSON.stringify(env.localEndPoint),
+          'production': env.production
+        }
+     })
+   ],
 };
